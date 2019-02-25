4c6e83f58c26dd145b23fb504bb4e7242f571057
Niraj Desai <niraj.desai@nio.io>
2019-02-25 13:28:53 -0800

    Convert from nanomsg to nng

--- conanfile.py
+++ conanfile.py
@@ -6,12 +6,12 @@ import os
 
 
 class NanomsgConan(ConanFile):
-    name = "nanomsg"
-    version = "1.1.2"
-    description = "Simple high-performance implementation of several scalability protocols"
-    topics = ("conan", "nanomsg", "communication", "messaging", "protocols")
-    url = "https://github.com/bincrafters/conan-nanomsg"
-    homepage = "https://github.com/nanomsg/nanomsg"
+    name = "nng"
+    version = "1.1.1"
+    description = "Light-weight brokerless messaging"
+    topics = ("conan", "nanomsg", "nng", "communication", "messaging", "protocols")
+    url = "https://github.com/ndesai/conan-nng"
+    homepage = "https://github.com/nanomsg/nng"
     author = "Bincrafters <bincrafters@gmail.com>"
     license = "MIT"
     exports = ["LICENSE.md"]
@@ -22,20 +22,21 @@ class NanomsgConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "enable_doc": [True, False],
-        "enable_getaddrinfo_a": [True, False],
         "enable_tests": [True, False],
         "enable_tools": [True, False],
         "enable_nanocat": [True, False],
+        "enable_coverage": [True, False],
+        "enable_tls": [True, False]
     }
     default_options = {
         'shared': False,
         'fPIC': True,
-        'enable_doc': False,
-        'enable_getaddrinfo_a': True,
         'enable_tests': False,
         'enable_tools': True,
-        'enable_nanocat': True
+        'enable_nanocat': True,
+        'enable_coverage': True,
+        'enable_tls': False,
+        
     }
     _source_subfolder = "source_subfolder"
 
@@ -47,19 +48,18 @@ class NanomsgConan(ConanFile):
         del self.settings.compiler.libcxx
 
     def source(self):
-        sha256 = "3c52165a735c2fb597d2306593ae4b17900688b90113d4115ad8480288f28ccb"
+        sha256 = "cec54ed40c8feb5c0c66f81cfd200e9b243639a75d1b6093c95ee55885273205"
         tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self._source_subfolder)
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        cmake.definitions["NN_STATIC_LIB"] = not self.options.shared
-        cmake.definitions["NN_ENABLE_DOC"] = self.options.enable_doc
-        cmake.definitions["NN_ENABLE_GETADDRINFO_A"] = self.options.enable_getaddrinfo_a
-        cmake.definitions["NN_TESTS"] = self.options.enable_tests
-        cmake.definitions["NN_TOOLS"] = self.options.enable_tools
-        cmake.definitions["NN_ENABLE_NANOCAT"] = self.options.enable_nanocat
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.definitions["NNG_TESTS"] = self.options.enable_tests
+        cmake.definitions["NNG_TOOLS"] = self.options.enable_tools
+        cmake.definitions["NNG_ENABLE_NNGCAT"] = self.options.enable_nanocat
+        cmake.definitions["NNG_ENABLE_COVERAGE"] = self.options.enable_coverage
         cmake.configure()
         return cmake
 
@@ -75,8 +75,8 @@ class NanomsgConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
 
-        if not self.options.shared:
-            self.cpp_info.defines.append("NN_STATIC_LIB=ON")
+        # if not self.options.shared:
+        #     self.cpp_info.defines.append("NN_STATIC_LIB=ON")
 
         if self.settings.os == "Windows" and not self.options.shared:
             self.cpp_info.libs.extend(['mswsock', 'ws2_32'])
